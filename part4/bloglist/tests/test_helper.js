1b04a94cbc6ed5df5f50c4dfeb9bda58da15776d
mostafashalaby <mshalaby2002@gmail.com>
2025-02-22 19:03:49 +0200

    Finished 4.17

--- part4/bloglist/tests/test_helper.js
+++ part4/bloglist/tests/test_helper.js
@@ -3,38 +3,35 @@ const User = require('../models/user')
 
 const initialBlogs = [
   {
-    _id: '5a422aa71b54a676234d17f9',
+    id: '5a422aa71b54a676234d17f9',
     title: 'Go To Statement Considered Harmful',
     author: 'Edsger W. Dijkstra',
     url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-    likes: 40,
-    __v: 0
+    likes: 40
   },
   {
-    _id: '5a422aa71b54a676234d17f8',
+    id: '5a422aa71b54a676234d17f8',
     title: 'Vroom?',
     author: 'Da Baby',
     url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-    likes: 10,
-    __v: 0
+    likes: 10
   },
 ]
 
 const nonExistingId = async () => {
   const blog = new Blog(
     {
-      _id: '80',
+      id: '80',
       title: 'Tester',
       author: 'Mr. Test',
       url: 'google.com',
       likes: 10000,
-      __v: 0
     },
   )
   await blog.save()
   await blog.deleteOne()
 
-  return Blog._id.toString()
+  return Blog.id.toString()
 }
 
 const blogsInDb = async () => {
