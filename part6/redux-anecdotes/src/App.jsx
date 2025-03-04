5c33707b4ee3853b833774f3b5f942bce86c494b
mostafashalaby <mshalaby2002@gmail.com>
2025-03-04 02:14:39 +0200

    6.3 through 6.8

---
+++ part6/redux-anecdotes/src/App.jsx
@@ -0,0 +1,16 @@
+
+import NewAnecdote from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
+
+
+const App = () => {
+
+  return (
+    <div>
+      <AnecdoteList />
+      <NewAnecdote />
+    </div>
+  )
+}
+
+export default App
